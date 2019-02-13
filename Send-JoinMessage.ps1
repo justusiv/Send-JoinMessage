d99bcf18be4aef5a6f426abf0ff5dc7282b4fa1f
justusiv <justusiv@hotmail.com>
2019-02-13 13:58:49 -0600

    Updated function to handle device names as well as device groups. also added a few veriables to handle feedback.

--- Send-JoinMessage.ps1
+++ Send-JoinMessage.ps1
@@ -8,8 +8,9 @@
     .EXAMPLE
         Send-JoinMessage -Apikey $myapikey -Title "MyTitle" -Text "MyText" -Group group.all
     .EXAMPLE
-        Send-JoinMessage $myapikey text2 title2 group.android
-
+        Send-JoinMessage $myapikey title2 text2 group.android
+    .EXAMPLE
+        Send-JoinMessage $myapikey title2 text2 group.android
     #>
     param(
     [Parameter(Position=0,mandatory=$true)]
@@ -24,11 +25,29 @@
     [ValidateLength(1,3200)]
     [ValidateNotNullOrEmpty()]
     [string]$Text,
-    [Parameter(Position=3,mandatory=$true)]
+    [Parameter(Position=3)]
     [ValidateSet("group.all","group.android","group.windows10","group.phone","group.tablet","group.pc")] 
     [ValidateNotNullOrEmpty()]
-    [string]$Group
+    [string]$Group,
+    [Parameter(Position=4)]
+    [string]$DeviceNames,
+    [Parameter()]
+    [bool]$displayURL = $false,
+    [Parameter()]
+    [bool]$displayResult = $true
     ) #end param
-
-    Invoke-WebRequest -Uri "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceId=$group&text=$text&title=$title&apikey=$apikey"
+    if ($DeviceNames.Length -gt 0){
+       $DeviceNamesURL = "deviceNames=$DeviceNames&"
+    }
+    if ($Group.Length -gt 0){
+       $GroupURL = "deviceId=$Group&"
+    }
+    if ($DeviceNames.Length -eq 0 -and $Group.Length -eq 0){
+        Write-Warning "You need to specify either a group or a device"
+        break
+    }
+    $fullWebRequest = "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?" + $DeviceNamesURL + $GroupURL + "text=" + $text + "&title=" + $title + "&apikey=" + $apikey
+    $response = Invoke-WebRequest -Uri $fullWebRequest
+    if ($displayURL){write-host $fullWebRequest}
+    if ($displayResult){write-host $response.content}
 }
