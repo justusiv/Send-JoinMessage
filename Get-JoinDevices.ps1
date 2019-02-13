cb46e8756a2a96db92960a0481803b82ce6a3644
justusiv <justusiv@hotmail.com>
2019-02-13 10:58:00 -0600

    Get-JoinDevices

---
+++ Get-JoinDevices.ps1
@@ -0,0 +1,22 @@
+﻿Function Get-JoinDevices
+{
+    <#
+    .SYNOPSIS
+        Get-JoinDevices can be used with the Join service to get the list of your devices.
+    .DESCRIPTION
+        
+    .EXAMPLE
+        Get-JoinDevices -Apikey $myapikey
+    .EXAMPLE
+        Send-JoinMessage $myapikey text2 title2 group.android
+
+    #>
+    param(
+    [Parameter(Position=0,mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateLength(32,32)]
+    [string]$Apikey
+    ) #end param
+$jsonobject = (Invoke-WebRequest https://joinjoaomgcd.appspot.com/_ah/api/registration/v1/listDevices?apikey=$myapikey).content | ConvertFrom-Json
+$jsonobject.records | ft id, deviceId, deviceName
+}
